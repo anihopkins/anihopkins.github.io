4154b0aa3ab2c02843aceee6a3b6d4006f15bb66
Ani <ani.d.hopkins@gmail.com>
2018-08-03 14:11:53 -0400

    Contact page now resizes on window resize. Added menu button, but did not connect to anything yet.

--- contact.js
+++ contact.js
@@ -1,6 +1,17 @@
-console.log($(window).width())
+// Resizes the contact window to accommodate smaller screens
+function resizeHandler() {
+  console.clear()
+  console.log($(window).width())
 
-if ($(window).width() < 400) {
-  $("#sidebar").css("display", "none")
-  $("#contactMain").css("left", "2%")
+  if ($(window).width() < 380) {
+    $("#sidebar").hide()
+    $("#menu").show()
+    $("#contactMain").css("left", "2%")
+    $("#contactMain p").css({"font-size":"100%", "margin-top":"1%"})
+  } else {
+    $("#sidebar").show()
+    $("#menu").hide()
+    $("#contactMain").css("left", "8em")
+    $("#contactMain p").css({"font-size":"120%", "margin-top":"0%"})
+  }
 }
