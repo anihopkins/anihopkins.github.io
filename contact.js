f693b28bd113c1ac202e099f12ac561f17b2c716
Ani <ani.d.hopkins@gmail.com>
2018-08-06 12:32:34 -0400

    Fixed contact page menu hover animation

--- contact.js
+++ contact.js
@@ -11,16 +11,18 @@ function resizeHandler() {
     $("h2").css("padding-left","0.5em")
     $("#contactMain").css("left", "2%")
     $("#contactMain p").css({"font-size":"100%", "margin-top":"1%"})
-    $("#sidebar").css({"position":"absolute", "left":"-240%", "top":"0%", "width":"9em", "z-index":"10000"})
-    $("#sidebar a").css({"font-size":"24pt", "border":"1px solid black","width":"9em"})
+    $("#sidebar").css({"position":"absolute", "left":"-240%", "top":"0%", "width":"4em", "z-index":"10000"})
+    $("#sidebar a").css({"font-size":"24pt", "border":"1px solid black","width":"4em"})
+    $("#sidebar a:hover").css({"width":"9em", "z-index":"10001"})
+
   } else {
     hideMenu()
     $("#menu").hide()
     $("h2").css("padding-left","1em")
     $("#contactMain").css("left", "8em")
     $("#contactMain p").css({"font-size":"120%", "margin-top":"0%"})
-    $("#sidebar").css({"position":"relative", "left":"1.3%", "top":"250%", "width":"9em", "z-index":"9999"})
-    $("#sidebar a").css({"font-size":"18pt", "border":"none","width":"9em"})
+    $("#sidebar").css({"position":"relative", "left":"1.3%", "top":"250%", "width":"4em", "z-index":"9999"})
+    $("#sidebar a").css({"font-size":"18pt", "border":"none","width":"4em"})
     $("#sidebar").show()
     $("#contactMain").before($("#sidebar"))
   }
