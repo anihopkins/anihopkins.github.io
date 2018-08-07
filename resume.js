035e3355ba990a0f7766972948178dbe58744a1e
Ani <ani.d.hopkins@gmail.com>
2018-08-07 12:39:18 -0400

    Finished responsiveness update for Resume page

--- resume.js
+++ resume.js
@@ -11,10 +11,11 @@ function resizeHandler() {
       $("#sidebar").hide()
       $("#menu").show()
       $("#sidebar a").css({"font-size":"24pt", "border":"1px solid black","width":"4em"})
-      $("#sidebar a:hover").css({"width":"9em", "z-index":"10001"})
+      $("#sidebar a:hover").css({"width":"4em", "z-index":"10001"})
       $("h2").css("width","70vw")
       windowSize = "Small"
     }
+    $(".workplace").css("font-size","100%")
     $("#resumeMain").css("left", "2%")
     $("#resumeContent").css({"font-size":"100%", "margin-top":"1%"})
     $("#sidebar").css({"position":"absolute", "left":"-220%", "top":"0%", "width":"4em", "z-index":"10000"})
@@ -28,10 +29,13 @@ function resizeHandler() {
       $("#resumeMain").before($("#sidebar"))
       windowSize = "Large"
     }
+    $(".workplace").css("font-size","120%")
     $("#resumeContent").css({"font-size":"120%", "margin-top":"0%"})
     screenWidth = $(window).width()/parseFloat($("#resumeMain").css("font-size"));
     mainWidth = (0.8 * (screenWidth - 12)).toString() + "em";
+    workplaceWidth = (0.7 * (screenWidth - 12)).toString() + "em";
     h2Width = (0.4 * (screenWidth - 6)).toString() + "em";
+    $(".workplace li").css("width", workplaceWidth)
     $("#resumeContent").css("width", mainWidth)
     $("h2").css("width", h2Width)
     $("#sidebar").css({"position":"relative", "left":"1em", "top":"250%", "width":"4em", "z-index":"9999"})
