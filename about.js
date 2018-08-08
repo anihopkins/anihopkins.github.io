77c1eecf870b26c6c6540a40ad5aba58250dc6ba
Ani <ani.d.hopkins@gmail.com>
2018-08-08 13:35:35 -0400

    Response design now implemented on all pages

--- about.js
+++ about.js
@@ -1,23 +1,32 @@
 $("#clickAway").hide()
 windowSize = "Large"
+resizeHandler()
 
 // Resizes the about window to accommodate smaller screens
 function resizeHandler() {
   /* console.clear()
   console.log($(window).width()) */
 
-  if ($(window).width() < 380) {
+  windowWidth = $(window).width()
+  textHeight = $("#aboutContent").height()
+  textWidth = $("#aboutContent").width()
+  photoWidth = $("#aboutPhoto").width()
+  photoLeft = (textWidth - photoWidth) * 0.5
+
+  if (windowWidth < 485) {
     if (windowSize == "Large") {
       $("#sidebar").hide()
       $("#menu").show()
       $("#sidebar a").css({"font-size":"24pt", "border":"1px solid black","width":"4em"})
       $("#sidebar a:hover").css({"width":"4em", "z-index":"10001"})
       $("h2").css("width","70vw")
+      $("h1").css("width","70%")
       windowSize = "Small"
     }
     $("#aboutMain").css("left", "2%")
     $("#aboutContent").css({"font-size":"100%", "margin-top":"1%"})
     $("#sidebar").css({"position":"absolute", "left":"-220%", "top":"0%", "width":"4em", "z-index":"10000"})
+    $("#aboutPhoto").css({"height":"auto", "width":0.75 * textWidth, "left":photoLeft, "top":textHeight, "display":"block", "position":"relative"})
   } else {
     if (windowSize = "Small") {
       hideMenu()
@@ -27,18 +36,24 @@ function resizeHandler() {
       $("#sidebar").show()
       $("#aboutMain").before($("#sidebar"))
       windowSize = "Large"
+      $("h1").css("width","100%")
     }
     $("#aboutContent").css({"font-size":"120%", "margin-top":"0%"})
-    screenWidth = $(window).width()/parseFloat($("#aboutMain").css("font-size"));
-    mainWidthNum = 0.9 * (screenWidth - 35);
-    mainWidth = (mainWidthNum).toString() + "em";
+    screenWidth = windowWidth/parseFloat($("body").css("font-size"));
+    mainWidth = (0.77 * (screenWidth - 35));
     h2Width = (0.4 * (screenWidth - 6)).toString() + "em";
-    console.clear()
-    console.log(mainWidthNum)
-    //photoLeft = (mainWidthNum).toString() + "em";
-    //$("#aboutPhoto").css("left", photoLeft)
-    console.log(photoLeft)
-    $("#aboutContent").css("width", mainWidth)
+    if (windowWidth > 750) {
+      $("#aboutPhoto").css("left", ((mainWidth * 1.3) + 1).toString() + "em")
+      $("#aboutContent").css("width", mainWidth.toString() + "em")
+      $("#aboutMain").prepend($("#aboutContent"))
+      $("#aboutPhoto").css({"width":"auto", "height":"20em","top":"0.5em", "display":"block", "position":"relative"})
+
+    } else {
+      console.log(textHeight)
+      $("#aboutContent").css({"width":"80%", "display":"block"})
+      $("#aboutPhoto").before($("#aboutContent"))
+      $("#aboutPhoto").css({"height":"auto", "width":0.75 * textWidth, "left":photoLeft, "top":textHeight+5, "display":"block", "position":"relative"})
+    }
     $("h2").css("width", h2Width)
     $("#sidebar").css({"position":"relative", "left":"1em", "top":"250%", "width":"4em", "z-index":"9999"})
   }
